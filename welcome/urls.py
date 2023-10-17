64c3bfacaa9986b8bf6bf7d9b49520fca084822c
DanishSharma1 <122099980+DanishSharma1@users.noreply.github.com>
2023-10-18 01:33:14 +0530

    Update urls.py

--- welcome/urls.py
+++ welcome/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('index/',views.index,name="index"),
+    path('',views.index,name="index"),
     #path('',views.xyz),
-    path('index/add/',views.add, name='add'),
-    path('index/add/hotel/',views.hotel,name='hotel'),
-    path('index/add/hotel/addrecord/',views.addrecord, name='addrecord'),
-    path('index/add/delete/<int:id>',views.delete,name='delete'),
-    path('index/add/update/<int:id>',views.update,name='update'),
-    path('index/add/update/updaterecord/<int:id>',views.updaterecord,name='updaterecord'),
+    path('add/',views.add, name='add'),
+    path('add/hotel/',views.hotel,name='hotel'),
+    path('add/hotel/addrecord/',views.addrecord, name='addrecord'),
+    path('add/delete/<int:id>',views.delete,name='delete'),
+    path('add/update/<int:id>',views.update,name='update'),
+    path('add/update/updaterecord/<int:id>',views.updaterecord,name='updaterecord'),
 
 ]
