a36a2973d95aa7f4e99fb5b2fd04089dd327c643
DanishSharma1 <122099980+DanishSharma1@users.noreply.github.com>
2023-10-19 11:31:02 +0530

    Eight Commit

--- django_project1/settings.py
+++ django_project1/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'welcome.apps.WelcomeConfig',
-    'csp'
+    
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   
+    
 ]
 
 ROOT_URLCONF = 'django_project1.urls'
